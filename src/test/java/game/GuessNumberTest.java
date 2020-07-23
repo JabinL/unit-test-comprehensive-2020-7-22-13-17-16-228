b77c1cb18bd1bef53ea6c45840b93e79067f45b3
Jabin <2099008974@qq.com>
2020-07-23 22:41:33 +0800

    [guess number][Jabin] test:should_return_0A0B_when_guess_given_inputNumber_5678

--- src/test/java/game/GuessNumberTest.java
+++ src/test/java/game/GuessNumberTest.java
@@ -22,4 +22,20 @@ public class GuessNumberTest {
         //then
         assertEquals("4A0B",result);
     }
+
+    @Test
+    public void should_return_0A0B_when_guess_given_inputNumber_5678(){
+        //given
+        int[] answer = {1,2,3,4};
+        int[] inputNumber = {5,6,7,8};
+        //stub
+        Generator numberGenerator = mock(NumberGenerator.class);
+        when(numberGenerator.generate()).thenReturn(inputNumber);
+        GuessNumber guessNumber = new GuessNumber(numberGenerator);
+        //when
+        String result = guessNumber.guess(inputNumber);
+        //then
+        assertEquals("0A0B",result);
+    }
+
 }
