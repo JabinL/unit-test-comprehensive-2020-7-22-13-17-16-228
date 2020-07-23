c0cd0948a48cdd418f2cd1abfc93d45aea876c61
Jabin <2099008974@qq.com>
2020-07-23 23:38:27 +0800

    [guess number][Jabin] refactor: finish generate method in NumberGenerator

--- src/main/java/game/NumberGenerator.java
+++ src/main/java/game/NumberGenerator.java
@@ -3,6 +3,29 @@ package game;
 public class NumberGenerator implements Generator{
     @Override
     public int[] generate() {
-        return null;
+        int n = 4;
+        int max = 9;
+        int min = 0;
+        
+        if (n > (max - min + 1) || max < min) {
+            return null;
+        }
+        int[] answer = new int[n];
+        int count = 0;
+        while(count < n) {
+            int num = (int) (Math.random() * (max - min)) + min;
+            boolean flag = true;
+            for (int j = 0; j < n; j++) {
+                if(num == answer[j]){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                answer[count] = num;
+                count++;
+            }
+        }
+        return answer;
     }
 }
